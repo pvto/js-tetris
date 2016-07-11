302759967810908f085ee2f1ce1bbf36943e7715
Paavo Toivanen <paavo.v.toivanen@gmail.com>
2016-07-11 22:34:50 +0300

    removed static server

--- server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = 8080;
-console.log("opening server at port " + port);
-connect().use(serveStatic(__dirname)).listen(port);
